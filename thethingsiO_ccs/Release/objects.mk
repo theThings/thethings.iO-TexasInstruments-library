0c5f937c62a068f10f18589680a34a9fa3dcf765
boasdi <zzzzeug@gmail.com>
2016-06-08 09:56:53 +0200

    Added documentation

--- thethingsiO_ccs/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/TI/CC3200SDK_1.2.0/cc3200-sdk/example/http_client_demo/ccs/../../../driverlib/ccs/Release/driverlib.a" -l"C:/TI/CC3200SDK_1.2.0/cc3200-sdk/example/http_client_demo/ccs/../../../simplelink/ccs/NON_OS/simplelink.a" -lwebclient.a -ljson.a
-
